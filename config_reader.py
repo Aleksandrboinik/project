e59119055427944b0a5e2aab056bc797a3524400
Aleksandrboinik <151654182+Aleksandrboinik@users.noreply.github.com>
2024-04-30 18:33:24 +0400

    Add files via upload

---
+++ config_reader.py
@@ -0,0 +1,17 @@
+import toml
+
+
+def read_config(file_path):
+    with open(file_path, "r") as file:
+        config = toml.load(file)
+    return config
+
+
+config = read_config("config.toml")
+
+token = config["telegram"]["token"]
+mongodb_server = config["mongodb"]["server"]
+mongodb_port = config["mongodb"]["port"]
+host = config["server"]["host"]
+port = config["server"]["port"]
+address = config["server"].get("address", None)
